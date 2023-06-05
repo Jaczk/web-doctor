cc1ff70f79ade5ad434e5998e3ba3be56610418f
Chrisz <cjmulyawan@gmail.com>
2023-06-05 12:04:59 +0700

    add foreign key to transactions

---
+++ database/migrations/2023_06_05_044256_add_foreign_key_to_transaction_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transaction', function (Blueprint $table) {
+            $table->foreign('appointment_id', 'fk_transaction_to_appointment')
+            ->references('id')->on('appointment')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transaction', function (Blueprint $table) {
+            $table->dropForeign('fk_transaction_to_appointment');
+        });
+    }
+};
