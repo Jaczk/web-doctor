cc28c6ac44e6c263b23595e29f6b2c59da2a6185
Chrisz <test4hack4l1@gmail.com>
2023-09-19 12:29:57 +0700

    add Frontsite Controller V1.0

---
+++ app/Http/Controllers/Frontsite/AppointmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Frontsite;
+
+use App\Http\Controllers\Controller;
+
+//use library
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Request;
+
+//use everything
+use Gate;
+use File;
+use Auth;
+
+//model here
+use App\Models\User;
+use App\Models\Operational\Doctor;
+use App\Models\MasterData\Consultation;
+
+//third party package
+
+class AppointmentController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Frontsite/PaymentController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Frontsite;
+
+use App\Http\Controllers\Controller;
+
+//use library
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Request;
+
+//use everything
+use Gate;
+use File;
+use Auth;
+
+//model here
+use App\Models\User;
+use App\Models\Operational\Doctor;
+use App\Models\Operational\Appointment;
+use App\Models\Operational\Transaction;
+use App\Models\MasterData\Consultation;
+use App\Models\MasterData\Specialist;
+use App\Models\MasterData\ConfigPayment;
+
+
+//third party package
+
+class PaymentController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.frontsite.payment.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return abort(404);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return abort(404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return abort(404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return abort(404);
+    }
+}
